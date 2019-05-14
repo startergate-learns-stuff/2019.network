6b749d460f5953c57a788265e69322fc9f4011e4
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-05-14 16:23:33 +0900

    5.14

---
+++ 5.14/cookieTestServer.js
@@ -0,0 +1,55 @@
+/*jshint esversion: 9 */
+
+const http = require('http');
+
+//var server = http.createServer((req, res) => {
+/*res.writeHead(200, {
+  'Set-Cookie': 'myCookie=fdsd'
+});
+res.writeHead(200, {
+  'Set-Cookie': ['cookie1=choco', 'cookie2=strawberry']
+});*/
+
+//res.writeHead(200, {
+//  'Set-Cookie': ['cookie1=choco', 'cookie2=strawberry', `permanent=cookie!; Max-Age=${60}`]
+//});
+
+//res.end('<h2>Cookie!</h2>');
+//}).listen(1337, _ => {
+//console.log(1337);
+//});
+
+const fs = require('fs');
+const cookie = require('cookie');
+const qs = require('querystring');
+const url = require('url');
+
+var server = http.createServer((req, res) => {
+  var cookies;
+  if (req.headers.cookie !== undefined) {
+    cookies = cookie.parse(req.headers.cookie);
+    console.log(cookies);
+  }
+
+  if (req.url.startsWith('/setCookie')) {
+    var parsedUrl = url.parse(req.url);
+    var query = qs.parse(parsedUrl.query);
+    res.writeHead(302, {
+      'Set-Cookie': `name=${query.name}`,
+      'Location': '/'
+    });
+    res.end();
+    return;
+  }
+
+  if (cookies.name) {
+    res.end(`welcome, ${cookies.name}`)
+    return;
+  }
+
+  fs.readFile('./cookieTest.html', (err, data) => {
+    res.end(data);
+  });
+}).listen(1337, _ => {
+  console.log(1337);
+});
--- 5.8/dramaServer.js
+++ 5.8/dramaServer.js
@@ -13,7 +13,7 @@ var dramaList = [{
   }
 ];
 
-const url = require('url');
+/*const url = require('url');
 var server = http.createServer((req, res) => {
   console.log('req.url: ', req.url);
 
@@ -24,10 +24,71 @@ var server = http.createServer((req, res) => {
   console.log('querystring ', query);
 
   // 드라마 목록에 데이터 추가
-  dramaList
-  //드라마 목록을 화면에 출력
-
-
+  if (query.title && query.actor) {
+    dramaList.push({
+      title: query.title,
+      actor: query.actor
+    });
+    res.writeHead(302, {
+      'Location': '/'
+    });
+    res.end();
+  } else {
+    showDramaList(res);
+  }
 }).listen(1337, _ => {
-  console.log('1337  포트에서 대기 중');
+  console.log('1337 포트에서 대기 중');
+});*/
+
+var showDramaList = res => {
+  //드라마 목록을 화면에 출력
+  res.writeHead(200, {
+    'Content-Type': 'text/html;charset=utf-8'
+  });
+  res.write(`<html><body><h1>My Favorite Drama</h1><ul>`);
+  dramaList.forEach(item => {
+    res.write(`<li>${item.title}(${item.actor})</li>`);
+  });
+  res.end(`</ul><h2>Add Drama</h2>
+  <form action="http://localhost:1337" method="POST">
+    <p>제목<input type="text" name="title"></p>
+    <p>배우<input type="text" name="actor"></p>
+    <input type="submit">
+  </form></body></html>`);
+};
+
+var addDramaList = (req, res) => {
+  var body = '';
+  req.on('data', chunk => {
+    body += chunk;
+  });
+  req.on('end', _ => {
+    var query = qs.parse(body);
+    // 드라마 목록에 데이터 추가
+    if (query.title && query.actor) {
+      dramaList.push({
+        title: query.title,
+        actor: query.actor
+      });
+      res.writeHead(302, {
+        'Location': '/'
+      });
+    } else {
+      res.writeHead(304, {
+        'Location': '/'
+      });
+    }
+    res.end();
+  });
+};
+
+var server = http.createServer((req, res) => {
+  // body에서 데이터 추출하여 저장
+  if (req.method.toLowerCase() === 'post') {
+    addDramaList(req, res);
+  } else {
+    showDramaList(res);
+  }
+}).listen(1337, _ => {
+  console.log(1337);
 });
